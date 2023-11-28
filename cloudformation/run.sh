017d2c48a819174b6de00f837ec21e8181ae59e0
Nick Chao <yc6371@nyu.edu>
2023-11-28 05:03:04 +0000

    run script support two github tokens

--- cloudformation/run.sh
+++ cloudformation/run.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-GITHUB_AUTH_TOKEN=${1}
+
+# create an array of two github oauth tokens that take from the command line
+GITHUB_AUTH_TOKEN_1=$1
+GITHUB_AUTH_TOKEN_2=$2
+GITHUB_AUTH_TOKENS=($GITHUB_AUTH_TOKEN_1 $GITHUB_AUTH_TOKEN_2)
 
 STACK_NAME_P1="P1"
 STACK_NAME_P2="P2"
-# create an array of stack names "P1" and "P2"
 STACK_NAMES=($STACK_NAME_P1 $STACK_NAME_P2)
-# create a bucket array
+
 BUCKET_NAMES=("p1-bucket-11-26" "p2-bucket-11-26" "aws-ai-album-11-26")
 set -eu
 
@@ -22,6 +25,7 @@ if [ "$1" == "-d" ]; then
                 # if the bucket is empty, then delete the bucket
                 aws s3 rb s3://$BUCKET_NAME --force || echo "no bucket"
         done
+        aws cloudformation delete-stack --stack-name P1-LF
         for STACK_NAME in "${STACK_NAMES[@]}"
         do
                 echo "Deleting stack...${STACK_NAME}"
@@ -36,23 +40,23 @@ else
         fi
 
         # loop through STACK_NAMES to check if the stack exists
-        for STACK_NAME in "${STACK_NAMES[@]}"
+        for idx in "${!STACK_NAMES[@]}"
         do
-                if aws cloudformation describe-stacks --stack-name $STACK_NAME > /dev/null 2>&1
+                if aws cloudformation describe-stacks --stack-name ${STACK_NAMES[$idx]} > /dev/null 2>&1
                 then
                         echo "Updating existing stack..."
                         aws cloudformation update-stack \
                                 --capabilities CAPABILITY_IAM \
-                                --stack-name $STACK_NAME \
-                                --parameters ParameterKey=GitHubOAuthToken,ParameterValue=$GITHUB_AUTH_TOKEN \
-                                --template-body file://${STACK_NAME}.yaml
+                                --stack-name ${STACK_NAMES[$idx]} \
+                                --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_AUTH_TOKENS[$idx]} \
+                                --template-body file://${STACK_NAMES[$idx]}.yaml
                 else
                         echo "Creating new stack..."
                         aws cloudformation create-stack \
                                 --capabilities CAPABILITY_IAM \
-                                --stack-name $STACK_NAME \
-                                --parameters ParameterKey=GitHubOAuthToken,ParameterValue=$GITHUB_AUTH_TOKEN \
-                                --template-body file://${STACK_NAME}.yaml
+                                --stack-name ${STACK_NAMES[$idx]} \
+                                --parameters ParameterKey=GitHubOAuthToken,ParameterValue=${GITHUB_AUTH_TOKENS[$idx]} \
+                                --template-body file://${STACK_NAMES[$idx]}.yaml
                 fi
         done
 fi
