7e490127012c333b277317743ca1a214228f40c3
Nick Chao <yc6371@nyu.edu>
2023-12-01 22:19:30 +0000

    update apiClient

--- frontend/apigClient.js
+++ frontend/apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://up9d2k6bg6.execute-api.us-east-1.amazonaws.com/v1';
+    var invokeUrl = 'https://iffp0wf1o1.execute-api.us-east-1.amazonaws.com/v1';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
