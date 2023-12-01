c886b83a66b93c64ce9ff2f60fc551755bb29f9e
Jayesh Kumar <jayeshmotog@gmail.com>
2023-11-30 22:39:14 -0500

    Added Lambda functions

--- lambda_functions/LF1/lf1.py
+++ lambda_functions/LF1/lf1.py
@@ -1,8 +1,64 @@
 import json
+import boto3
+import requests
+import time
+from requests_aws4auth import AWS4Auth
+
+# Initialize S3 client
+s3_client = boto3.client('s3')
+
+def detect_labels(photo, bucket):
+    labels_res = []
+
+    client = boto3.client('rekognition')
+
+    response = client.detect_labels(Image={'S3Object':{'Bucket':bucket,'Name':photo}}, MaxLabels=10)
+    print('Detected labels for ' + photo)
+    for label in response['Labels']:
+        print("Label: " + label['Name'])
+        labels_res.append(label['Name'])
+
+    try:
+        im_metadata = s3_client.head_object(Bucket=bucket, Key=photo)
+        if 'Metadata' in im_metadata and 'customlabels' in im_metadata['Metadata']:
+            user_labels = im_metadata['Metadata']['customlabels'].split(",")
+            labels_res.extend(user_labels)
+    except Exception as e:
+        print(f"Error getting metadata for {photo}: {str(e)}")
+
+    return labels_res
 
 def lambda_handler(event, context):
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Hello World!"})
+    print(event)
+    bucket = "b2jk7792"
+    elastic_url = "https://search-photos-iqnht2dwn4nwyktpgelc2zhqeu.us-east-1.es.amazonaws.com"
+
+    region = 'us-east-1' 
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+
+    for record in event['Records']:
+        image_name = record["s3"]["object"]["key"]
+        labels_res = detect_labels(image_name, bucket)
+        print("labels result =====>>", labels_res)
+        timestamp = time.time()
+        query = {'objectKey': image_name, 'bucket': bucket, 'createdTimestamp': timestamp,'labels': labels_res}
+        # response = requests.post(elastic_url, auth= awsauth, data = json.dumps(query), headers= headers)
+        index_into_es(elastic_url,'photos',json.dumps(query), awsauth)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
     }
-    return response
+
+def index_into_es(elastic_url, index, new_doc, awsauth):
+    endpoint = f'{elastic_url}/{index}/_doc'  
+    print(endpoint)
+    headers = {'Content-Type': 'application/json'}
+    try:
+        res = requests.post(endpoint, auth=awsauth, data=new_doc, headers=headers)
+        print(res)
+    except Exception as e:
+        print(f"Error indexing document: {str(e)}")
+
--- lambda_functions/LF2/lf2.py
+++ lambda_functions/LF2/lf2.py
@@ -1,8 +1,107 @@
 import json
+import boto3
+import time
+# from botocore.vendored import requests
+import requests
+from requests_aws4auth import AWS4Auth
 
 def lambda_handler(event, context):
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({"message": "Hello World!"})
+    # TODO implement
+    print('********** event **********    ',event)
+  
+    # inputText = event["params"]["querystring"]["q"]
+    inputText = "Show me Woman"
+    print('inputText  :  ', inputText)
+    keywords = get_keywords(inputText)
+    print('keywords ', keywords)
+    # image_array = get_image_locations(keywords)
+    
+    if len(keywords) == 1:
+        if keywords[0][-1] == 's':
+            image_array = get_image_locations([str(keywords[0][0:-1])])[:2]
+            # image_array = image_array[:3]
+        elif keywords[0][-1] != 's':
+            image_array = get_image_locations(keywords)[0:1]
+            # image_array = image_array[0]
+    
+    if len(keywords) == 2:
+        if keywords[0][-1] == 's' and keywords[1][-1] == 's':
+            image_array = get_image_locations([str(keywords[0][0:-1])])[:2]
+            image_array_2 = get_image_locations([str(keywords[1][0:-1])])[:2]
+            image_array.extend(image_array_2)
+        
+        elif keywords[0][-1] == 's' and keywords[1][-1] != 's':
+            image_array = get_image_locations([str(keywords[0][0:-1])])[:2]
+            image_array_2 = get_image_locations([keywords[1]])[0:1]
+            image_array.extend(image_array_2)
+        
+        elif keywords[0][-1] != 's' and keywords[1][-1] == 's':
+            image_array = get_image_locations([keywords[0]])[0:1]
+            image_array_2 = get_image_locations([str(keywords[1][0:-1])])[:2]
+            image_array.extend(image_array_2)
+        
+        elif keywords[0][-1] != 's' and keywords[1][-1] != 's':
+            image_array = get_image_locations([keywords[0]])[0:1]
+            image_array_2 = get_image_locations([keywords[1]])[0:1]
+            image_array.extend(image_array_2)
+    
+    
+    return {
+        # 'check': event,
+        'statusCode': 200,
+        'headers':{
+            'Access-Control-Allow-Origin':'*',
+            'Access-Control-Allow-Credentials':True
+        },
+        'body': {"results": image_array} 
+            # json.dumps(
+            # # {"results":image_array}
+            # )
+        # 'body': json.dumps({"results":keywords})
     }
-    return response
+
+
+def get_keywords(inputstr):
+    print("before lex")
+    lex = boto3.client('lex-runtime')
+    response = lex.post_text(
+        botName = 'AISearchBot',
+        botAlias = 'AISearchBot',
+        userId = 'searchPhotosLambda',
+        inputText = inputstr
+    )
+    # print(response)
+    print(response['slots'])
+    print("after lex")
+    keywords = []
+    slots = response['slots']
+    keywords = [v for _, v in slots.items() if v]
+    print("lex returned keywords",keywords)
+    return keywords
+    
+def get_image_locations(keywords):
+    endpoint = 'https://search-photos-iqnht2dwn4nwyktpgelc2zhqeu.us-east-1.es.amazonaws.com/_search'
+    headers = {'Content-Type': 'application/json'}
+    
+    region = 'us-east-1' 
+    service = 'es'
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, region, service, session_token=credentials.token)
+    
+    prepared_q = []
+    for k in keywords:
+        prepared_q.append({"match": {"labels": k}})
+    q = {"query": {"bool": {"should": prepared_q}}}
+    r = requests.post(endpoint, auth = awsauth, headers=headers, data=json.dumps(q))
+    print(r.json())
+    # r = {"took": 7, "timed_out": False, "_shards": {"total": 5, "successful": 5, "skipped": 0, "failed": 0}, "hits": {"total": {"value": 4, "relation": "eq"}, "max_score": 1.0, "hits": [{"_index": "photos", "_type": "photo", "_id": "5uWzhW4B9mNrwci-n6la", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/gettyimages-91495990-170667a.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220611", "labels": ["Plant", "Tree", "Person", "Human", "Tree Trunk", "Outdoors"]}}, {"_index": "photos", "_type": "photo", "_id": "6OW5hW4B9mNrwci-Yqk8", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/scott-eastwood.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-221229", "labels": ["Person", "Human", "Canine", "Dog", "Pet", "Animal", "Mammal"]}}, {"_index": "photos", "_type": "photo", "_id": "5-W0hW4B9mNrwci-ZqlW", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/pjimage-43-2.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220702", "labels": ["Tree", "Christmas Tree", "Ornament", "Plant", "Human", "Person"]}}, {"_index": "photos", "_type": "photo", "_id": "5eWzhW4B9mNrwci-V6mF", "_score": 1.0, "_source": {"objectKey": "uploaded_photos/pjimage-43-2.jpg", "bucket": "photosb2", "createdTimestamp": "20191119-220553", "labels": ["Tree", "Ornament", "Christmas Tree", "Plant", "Person", "Human"]}}]}}
+    
+    image_array = []
+    for each in r.json()['hits']['hits']:
+        objectKey = each['_source']['objectKey']
+        bucket = each['_source']['bucket']
+        image_url = "https://" + bucket + ".s3.amazonaws.com/" + objectKey
+        image_array.append(image_url)
+        print(each['_source']['labels'])
+    print(image_array)
+    return image_array
